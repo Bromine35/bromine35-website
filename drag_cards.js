95710ce2ae550d460928bb1e00dbe85f7680686d
appleflyer <theappleflyer@icloud.com>
2024-05-23 15:44:11 +0800

    additional drag fixes

--- drag_cards.js
+++ drag_cards.js
@@ -1,11 +1,21 @@
-// insane o ma gaw card dragging
 document.addEventListener('DOMContentLoaded', (event) => {
   const containers = document.querySelectorAll('#draggable-cards-container');
   const draggable = new Draggable.Sortable(containers, {
-    draggable: '.col'
+    draggable: '.col',
+    delay: 150, // delay fix so when clicking button dont accidentally drag
+    // fix miror image slipping away/not in sync wiht the cursor with setting some shits
+    mirror: {
+      constrainDimensions: true, 
+      cursorOffsetX: 0,
+      cursorOffsetY: 0,
+      appendTo: 'body',
+      xAxis: true,
+      yAxis: true 
+    }
   });
-  // just leave the console.logs here ok i want it there shut up
-  draggable.on('sortable:start', () => console.log('Drag started')); //debugging shit
-  draggable.on('sortable:sort', () => console.log('Dragging')); // debug poop
-  draggable.on('sortable:stop', () => console.log('Drag stopped')); //debug for stop
+
+  draggable.on('sortable:start', () => console.log('Drag started'));
+  draggable.on('sortable:sort', () => console.log('Dragging'));
+  draggable.on('sortable:stop', () => console.log('Drag stopped'));
 });
+
